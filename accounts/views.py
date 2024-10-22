24d9779c5b0108a885322bf86d7dc823ddce7e73
Jithininmakes <jithinbalqn@gmail.com>
2024-10-22 12:10:57 +0530

    views.py updated for login page

--- accounts/views.py
+++ accounts/views.py
@@ -4,20 +4,31 @@ from django.views.generic import TemplateView
 
 
 from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.views.generic import CreateView,FormView
 from django.contrib.auth import login
 from django.shortcuts import redirect
-from .forms import RegisterForm
+from .forms import RegisterForm,LoginForm
 
 class RegisterView(CreateView):
     form_class = RegisterForm
     template_name = 'school/register.html'
-    success_url = reverse_lazy('home')  # Redirect to login after registration
+    success_url = reverse_lazy('login')  # Redirect to login after registration
 
     def form_valid(self, form):
         user = form.save()
         login(self.request, user)  # Log in the user after registration
-        return redirect('home')  # Redirect to home or another page after registration
+        return redirect('login')  # Redirect to home or another page after registration
+
+
+
+class LoginView(FormView):
+    template_name = 'school/login.html'
+    form_class = LoginForm
+    success_url = reverse_lazy('home')  # Redirect to home after login
+
+    def form_valid(self, form):
+        login(self.request, form.get_user())  # Log in the user
+        return super().form_valid(form)
 
 
 # Create your views here.
