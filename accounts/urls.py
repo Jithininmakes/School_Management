7d4eeceb7240b2c28b53f1ac26a80cf630a9ab56
Jithininmakes <jithinbalqn@gmail.com>
2024-10-22 12:12:00 +0530

    login.html created for  login page

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import HomeView,RegisterView
+from .views import HomeView,RegisterView,LoginView
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('register', RegisterView.as_view(), name='register'),
+    path('login', LoginView.as_view(), name='login'),
+
 
 ]
