7a15fe631acbc6febc5bfb4328d4e7987da10783
Jithininmakes <jithinbalqn@gmail.com>
2024-10-23 14:10:22 +0530

    urls updated in student app

--- student/urls.py
+++ student/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
+from .views import StudentListView, StudentCreateView, StudentUpdateView, StudentDeleteView
 
 app_name = 'student'
 
+
 urlpatterns = [
-    
+    path('list', StudentListView.as_view(), name='student-list'),
+    path('create/', StudentCreateView.as_view(), name='student-create'),
+    path('update/<int:pk>/', StudentUpdateView.as_view(), name='student-update'),
+    path('delete/<int:pk>/', StudentDeleteView.as_view(), name='student-delete'),
 ]
