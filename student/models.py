ddcb30deaf7e40f0f04b9f97fa4f3e50d90fbfcd
Jithininmakes <jithinbalqn@gmail.com>
2024-10-23 14:01:19 +0530

    models created in student app

--- student/models.py
+++ student/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Student(models.Model):
+    name = models.CharField(max_length=255)
+    dob = models.DateField()
+    class_name = models.CharField(max_length=50)
+    guardian_name = models.CharField(max_length=255)
+    address = models.TextField()
