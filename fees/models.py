4a23e58517c563066372a9dd26e5fc5aafa2b63e
Jithininmakes <jithinbalqn@gmail.com>
2024-10-24 10:42:24 +0530

    models updated in fees app

--- fees/models.py
+++ fees/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from student.models import Student
 
-# Create your models here.
+class Fee(models.Model):
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    date_paid = models.DateField()
+
+    def __str__(self):
+        return f'{self.student.name} - {self.amount}'
