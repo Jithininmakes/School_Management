d7a775fb11c833ca0b04476763eb4a00602ed4e8
Jithininmakes <jithinbalqn@gmail.com>
2024-10-24 09:53:25 +0530

    models  created library app

--- library/models.py
+++ library/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from student.models import Student
 
-# Create your models here.
+class Library(models.Model):
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    book_title = models.CharField(max_length=200)
+    issue_date = models.DateField()
+    return_date = models.DateField(null=True, blank=True)
+
+    def __str__(self):
+        return self.book_title
