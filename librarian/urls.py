4710f788bb05fe54e1e2ae0da89d9ef99e9ccd9d
Jithininmakes <jithinbalqn@gmail.com>
2024-10-24 15:39:05 +0530

    views and urls updated in librarian app

--- librarian/urls.py
+++ librarian/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
+from .views import LibrarianCreateView, LibrarianListView, LibrarianUpdateView, LibrarianDeleteView
+
+
 app_name = 'librarian'
 
 urlpatterns = [
-    
+     path('list/', LibrarianListView.as_view(), name='librarian_list'),
+    path('add/', LibrarianCreateView.as_view(), name='librarian_add'),
+    path('edit/<int:pk>/', LibrarianUpdateView.as_view(), name='librarian_edit'),
+    path('delete/<int:pk>/', LibrarianDeleteView.as_view(), name='librarian_delete'),
 ]
--- librarian/views.py
+++ librarian/views.py
@@ -1,3 +1,36 @@
 from django.shortcuts import render
+from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView, UpdateView, DeleteView, ListView
+from django.shortcuts import redirect
+from .models import Librarian
+from .forms import LibrarianForm
 
-# Create your views here.
+class LibrarianListView(ListView):
+    model = Librarian
+    template_name = 'librarian/librarian_list.html'
+    context_object_name = 'librarian_list'
+
+
+class LibrarianCreateView(CreateView):
+    model = Librarian
+    form_class = LibrarianForm
+    template_name = 'librarian/librarian_form.html'
+    success_url = reverse_lazy('librarian:librarian_list')
+
+    def form_valid(self, form):
+        return super().form_valid(form)
+
+
+class LibrarianUpdateView(UpdateView):
+    model = Librarian
+    form_class = LibrarianForm
+    template_name = 'librarian/librarian_form.html'
+    success_url = reverse_lazy('librarian:librarian_list')
+
+
+class LibrarianDeleteView(DeleteView):
+    model = Librarian
+    template_name = 'librarian/librarian_delete.html'
+    success_url = reverse_lazy('librarian:librarian_list')
+
